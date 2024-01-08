31557ea0b96f94a7137e30516526c80aed2b4221
hyun7520 <sanghyun123452@gmail.com>
2024-01-08 18:50:28 +0900

    initial commit
    initial setting, hello world print check

---
+++ src/main/java/com/fisrtproject/forum/service/BoardService.java
@@ -0,0 +1,11 @@
+package com.fisrtproject.forum.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+    public String hello() {
+        return "Hello World!";
+    }
+}

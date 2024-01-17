9a677990d4725fcc3293730c9e5f602e617bea14
hyun7520 <sanghyun123452@gmail.com>
2024-01-17 14:38:59 +0900

    Feat: 게시판 별 게시글 조회, 삭제

--- src/main/java/com/fisrtproject/forum/controller/PostController.java
+++ src/main/java/com/fisrtproject/forum/controller/PostController.java
@@ -2,13 +2,18 @@ package com.fisrtproject.forum.controller;
 
 import com.fisrtproject.forum.dto.PostPatchDto;
 import com.fisrtproject.forum.dto.PostRequestDto;
+import com.fisrtproject.forum.entity.BoardEntity;
 import com.fisrtproject.forum.entity.PostEntity;
+import com.fisrtproject.forum.service.BoardService;
 import com.fisrtproject.forum.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,14 +21,20 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 
     private final PostService postService;
+    private final BoardService boardService;
 
     @GetMapping("/{boardId}/posts")
-    public Page<PostEntity> getAllPosts(@PathVariable("boardId") Long id,
+    public String getAllPosts(Model model,
+                                        @PathVariable("boardId") Long id,
                                         @RequestParam(name = "page", defaultValue = "0") int page,
                                         @RequestParam(name = "size", defaultValue = "5") int size) {
         PageRequest pageRequest = PageRequest.of(page, size);
+        Page<PostEntity> foundPosts = postService.findPostsByBoard(id, pageRequest);
+        BoardEntity selectedBoard = boardService.findBoard(id);
+        model.addAttribute("posts", foundPosts);
+        model.addAttribute("board", selectedBoard);
 
-        return postService.findPostsByBoard(id, pageRequest);
+        return "showPosts";
     }
 
     @GetMapping("/{boardId}/posts/{postId}")
@@ -46,10 +57,24 @@ public class PostController {
                             @RequestBody PostPatchDto postPatchDto) {
         postService.updatePost(boardId, postId, postPatchDto);
     }
-
+    
+    // redirect 질문하기
     @DeleteMapping("/{boardId}/posts/{postId}/delete")
-    public void deletePost(@PathVariable("boardId") Long boardId,
-                           @PathVariable("postId") Long postId) {
+    public String deletePost(Model model,
+                             @PathVariable("boardId") Long boardId,
+                             @PathVariable("postId") Long postId,
+                             @RequestParam(name = "page", defaultValue = "0") int page,
+                             @RequestParam(name = "size", defaultValue = "5") int size) {
+
         postService.deletePost(boardId, postId);
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<PostEntity> foundPosts = postService.findPostsByBoard(boardId, pageRequest);
+        BoardEntity selectedBoard = boardService.findBoard(postId);
+
+        model.addAttribute("posts", foundPosts);
+        model.addAttribute("board", selectedBoard);
+
+        return "showPosts";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,8 +7,8 @@ VALUES
 
 INSERT INTO post (post_id, board_id, title, content, created_at)
 VALUES
-(1, 1, 'new cat', 'say hello!', NOW()),
-(2, 1, 'first post', 'happy new year', NOW()),
+(1, 1, 'just adopted cat', 'say hello!', NOW()),
+(2, 1, 'first post of new year!', 'happy new year', NOW()),
 (3, 2, 'busy day coding', 'need coffee', NOW()),
 (4, 2, 'all done!', 'Hello, World!', NOW());
 
